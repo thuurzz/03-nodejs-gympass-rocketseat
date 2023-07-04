464eb1a997161cca91320cb0b7ead0bd07e7d036
Arthur Vinicius <thuur.vss@gmail.com>
2023-07-04 17:01:13 -0300

    Caso de uso de autenticação

---
+++ src/use-case/authenticate.ts
@@ -0,0 +1,35 @@
+import { UserRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User;
+}
+
+export class Authenticate {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatch = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError();
+    }
+
+    return { user };
+  }
+}

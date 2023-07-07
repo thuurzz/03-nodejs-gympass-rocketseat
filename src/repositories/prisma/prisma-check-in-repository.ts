5e673e5af99a2e13cf0c3b6d8479c3347c7ef505
Arthur Vinicius <thuur.vss@gmail.com>
2023-07-06 21:04:31 -0300

    Repositórios do Prisma

---
+++ src/repositories/prisma/prisma-check-in-repository.ts
@@ -0,0 +1,58 @@
+import { CheckIn, Prisma } from "@prisma/client";
+import { CheckInsRepository } from "../check-ins-repository";
+import { prisma } from "@/lib/prisma";
+import dayjs from "dayjs";
+
+export class PrismaCheckInRepositoy implements CheckInsRepository {
+  async findById(checkInId: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: { id: checkInId },
+    });
+    return checkIn;
+  }
+
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({
+      where: { id: userId },
+    });
+    return count;
+  }
+
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: { id: userId },
+      take: 20,
+      skip: 20 * (page - 1),
+    });
+    return checkIns;
+  }
+
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = await prisma.checkIn.create({ data });
+    return checkIn;
+  }
+
+  async save(data: CheckIn) {
+    const checkIn = await prisma.checkIn.update({
+      where: { id: data.id },
+      data,
+    });
+    return checkIn;
+  }
+
+  async findByUserOnDate(userId: string, date: Date) {
+    const startOfDay = dayjs(date).startOf("date");
+    const endOfDay = dayjs(date).endOf("date");
+
+    const checkIn = await prisma.checkIn.findFirst({
+      where: {
+        id: userId,
+        created_at: {
+          gte: startOfDay.toDate(),
+          lte: endOfDay.toDate(),
+        },
+      },
+    });
+    return checkIn;
+  }
+}
